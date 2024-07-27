51176b83e8a917b4db3107c37a1c220ad4e3258d
mivium <mivium@wisebits.com>
2024-07-27 14:11:09 +0300

    step 3

---
+++ gendiff/__init__.py
@@ -0,0 +1,3 @@
+from gendiff.gendiff import *
+
+__all__ = ['generate_diff']
--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,4 +1,61 @@
 import argparse
+import json
+
+
+def _compare_jsons(json1, json2):
+    all_keys = set(json1.keys()).union(set(json2.keys()))
+
+    diff = []
+
+    for key in all_keys:
+        match (key in json1, key in json2):
+            case (True, True) if json1[key] != json2[key]:
+                diff.append({
+                    'type': 'changed',
+                    'key': key,
+                    'old_value': json1[key],
+                    'new_value': json2[key]
+                })
+            case (True, False):
+                diff.append({
+                    'type': 'removed',
+                    'key': key,
+                    'value': json1[key]
+                })
+            case (False, True):
+                diff.append({
+                    'type': 'added',
+                    'key': key,
+                    'value': json2[key]
+                })
+
+    return diff
+
+
+def _format_diff(diff):
+    lines = ["{"]
+    for diff in diff:
+        if diff['type'] == 'changed':
+            lines.append(f"  - {diff['key']}: {diff['old_value']}")
+            lines.append(f"  + {diff['key']}: {diff['new_value']}")
+        elif diff['type'] == 'removed':
+            lines.append(f"  - {diff['key']}: {diff['value']}")
+        elif diff['type'] == 'added':
+            lines.append(f"  + {diff['key']}: {diff['value']}")
+    lines.append("}")
+
+    return "\n".join(lines)
+
+
+def generate_diff(file_path1: str, file_path2: str) -> str:
+    with open(file_path1, 'r') as f1, open(file_path2, 'r') as f2:
+        data1 = json.load(f1)
+        data2 = json.load(f2)
+
+    diff = _compare_jsons(data1, data2)
+    formated_diff = _format_diff(diff)
+
+    return formated_diff
 
 
 def main():
@@ -20,8 +77,15 @@ def main():
         type=str,
         help='set format of output'
     )
+
     args = parser.parse_args()
+
+    diff_output = generate_diff(args.first_file, args.second_file)
+
+    print(diff_output)
 
 
 if __name__ == '__main__':
     main()
+
+__all__ = ['generate_diff']
